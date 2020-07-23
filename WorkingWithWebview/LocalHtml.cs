7d3904bf2765889c1ce634913594e7cb7a7dbfbd
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2020-07-23 14:48:19 +0800

    Load full project

---
+++ WorkingWithWebview/LocalHtml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class LocalHtml : ContentPage
+    {
+        public LocalHtml()
+        {
+            //Using Html to load embedded link of domain restricted YouTube Video will show "Video Unavailable" message.
+
+            var browser = new WebView();
+            var htmlSource = new HtmlWebViewSource();
+
+            //Embedded Domain Restricted YouTube Video, will get "Video Unavailable" error message
+            htmlSource.Html = @"<html>
+                                    <body>  
+                                        <div style=' position: relative; padding-bottom: 56.25%; padding-top: 25px; overflow: hidden;'> 
+                                            <iframe style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' src='https://www.youtube.com/embed/ssP0GrK8Uws' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+                                        </div> 
+                                    </body>
+                                </html>";
+
+            //Embedded Unrestricted YouTube video, works fine
+            //htmlSource.Html = @"<html>
+            //                        <body>  
+            //                            <div style=' position: relative; padding-bottom: 56.25%; padding-top: 25px; overflow: hidden;'> 
+            //                                <iframe style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' src='https://www.youtube.com/embed/dOgwQ5pmSiQ' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+            //                            </div> 
+            //                        </body>
+            //                    </html>";
+            browser.Source = htmlSource;
+            Content = browser;
+        }
+    }
+}
---
+++ WorkingWithWebview/WebPage.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+
+            //Using Url works fine (not embedded link, but normal Url)
+
+            //Original webpage of the sample
+            //browser.Source = "http://xamarin.com";
+
+            //Unrestricted YouTube video
+            //browser.Source = "https://www.youtube.com/watch?v=4OZip0cgOho";
+
+            //Domain Restricted YouTube video
+            browser.Source = "https://www.youtube.com/watch?v=ssP0GrK8Uws";
+
+            Content = browser;
+        }
+    }
+}
+
